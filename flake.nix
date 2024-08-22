1ac48fd2e96564a4eda536ae113e7ffe44b6e66c
ElXreno <elxreno@gmail.com>
2024-08-22 16:39:52 +0300

    {flake,shell}.nix: Reformat with nixfmt

--- flake.nix
+++ flake.nix
@@ -18,83 +18,74 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay, crate2nix, ... }:
-    let
-      name = "teledump";
-    in
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          # Imports
-          rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+    let name = "teledump";
+    in flake-utils.lib.eachDefaultSystem (system:
+      let
+        # Imports
+        rust-toolchain =
+          pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-          pkgsWithouOverlays = import nixpkgs {
-            inherit system;
-          };
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              rust-overlay.overlays.default
-              (self: super: {
-                rustc = rust-toolchain;
-                cargo = rust-toolchain;
-              })
-            ];
-          };
-          inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
-            generatedCargoNix;
-
-          # Project
-          project = pkgs.callPackage
-            (generatedCargoNix {
-              inherit name;
-              src = ./.;
+        pkgsWithouOverlays = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            rust-overlay.overlays.default
+            (self: super: {
+              rustc = rust-toolchain;
+              cargo = rust-toolchain;
             })
-            {
-              defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-                ${name} = oldAttrs: {
-                  inherit buildInputs nativeBuildInputs;
-                } // buildEnvVars;
-              };
-            };
+          ];
+        };
+        inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
+          generatedCargoNix;
 
-          buildInputs = with pkgs; [ openssl.dev sqlite ];
-          nativeBuildInputs = with pkgs; [ rustc cargo pkg-config ];
-          buildEnvVars = {
-            PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        # Project
+        project = pkgs.callPackage (generatedCargoNix {
+          inherit name;
+          src = ./.;
+        }) {
+          defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+            ${name} = oldAttrs:
+              {
+                inherit buildInputs nativeBuildInputs;
+              } // buildEnvVars;
           };
-        in
-        rec {
-          packages = {
-            ${name} = project.workspaceMembers.${name}.build;
-            default = packages.${name};
+        };
+
+        buildInputs = with pkgs; [ openssl.dev sqlite ];
+        nativeBuildInputs = with pkgs; [ rustc cargo pkg-config ];
+        buildEnvVars = {
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
+      in rec {
+        packages = {
+          ${name} = project.workspaceMembers.${name}.build;
+          default = packages.${name};
+        };
+
+        apps = {
+          ${name} = flake-utils.lib.mkApp {
+            inherit name;
+            drv = packages.${name};
           };
+          default = apps.${name};
+        };
 
-          apps = {
-            ${name} = flake-utils.lib.mkApp {
-              inherit name;
-              drv = packages.${name};
-            };
-            default = apps.${name};
-          };
+        checks = { ${name} = packages.${name}.override { runTests = true; }; };
 
-          checks = {
-            ${name} = packages.${name}.override { runTests = true; };
-          };
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs;
 
-          devShells.default = pkgs.mkShell
-            {
-              inherit buildInputs;
+          nativeBuildInputs = (with pkgsWithouOverlays; [ sea-orm-cli ])
+            ++ (with pkgs; [ rustfmt rust-analyzer lldb ]) ++ nativeBuildInputs;
 
-              nativeBuildInputs = (with pkgsWithouOverlays; [ sea-orm-cli ]) ++ (with pkgs; [ rustfmt rust-analyzer lldb ]) ++ nativeBuildInputs;
+          shellHook = ''
+            # For JetBrains CLion
+            # Set standart library path to: .rust-src/rust
+            ln -sfT ${rust-toolchain}/lib/rustlib/src ./.rust-src
+          '';
 
-              shellHook = ''
-                # For JetBrains CLion
-                # Set standart library path to: .rust-src/rust
-                ln -sfT ${rust-toolchain}/lib/rustlib/src ./.rust-src
-              '';
-
-              RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/src";
-            } // buildEnvVars;
-        }
-      );
+          RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/src";
+        } // buildEnvVars;
+      });
 }
